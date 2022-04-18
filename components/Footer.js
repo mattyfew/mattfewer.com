927fdb10da5a7173a8357531e4f19aa96d668d9c
mattyfew <matthew.fewer@takeaway.com>
2022-04-18 20:33:47 +0200

    favicon, about pic, email, zeno cred

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,38 @@
 export default function Footer() {
   const links = [
     {
-      title: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/mattfewer/',
-      icon: 'ri-linkedin-line'
+      title: "LinkedIn",
+      url: "https://www.linkedin.com/in/mattfewer/",
+      icon: "ri-linkedin-line"
     },
     {
-      title: 'GitHub',
-      url: 'https://github.com/mattyfew',
-      icon: 'ri-github-line'
+      title: "GitHub",
+      url: "https://github.com/mattyfew",
+      icon: "ri-github-line"
     },
     {
-      title: 'Instagram',
-      url: 'https://instagram.com/mattyfew',
-      icon: 'ri-instagram-line'
-    },
-  ]
+      title: "Instagram",
+      url: "https://instagram.com/mattyfew",
+      icon: "ri-instagram-line"
+    }
+  ];
 
-  return <footer className="footer">
-    {links.map((link, index) => {
-      return <a key={index} href={link.url} target="_blank">
-        <span>{link.title}</span>
-        <i className={link.icon} />
-      </a>
-    })}
-  </footer>
+  return (
+    <>
+      <footer className="footer">
+        {links.map((link, index) => {
+          return (
+            <a key={index} href={link.url} target="_blank">
+              <span>{link.title}</span>
+              <i className={link.icon} />
+            </a>
+          );
+        })}
+      </footer>
+      <p className="website-credit">
+        website based on{" "}
+        <a href="https://github.com/zenorocha/zenorocha.com">zenorocha.com</a>
+      </p>
+    </>
+  );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,53 +1,56 @@
-import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import { parseISO, format, intervalToDuration } from 'date-fns'
-import Main from '../layouts/Main'
-import stripHtml from '../lib/strip-html'
-import career from '../data/about'
-import videos from '../data/videos'
-import FeaturedContent from '../components/FeaturedContent'
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { parseISO, format, intervalToDuration } from "date-fns";
+import Main from "../layouts/Main";
+import stripHtml from "../lib/strip-html";
+import career from "../data/about";
+import videos from "../data/videos";
+import FeaturedContent from "../components/FeaturedContent";
 
 export async function getStaticProps() {
   const meta = {
-    title: 'About | Matt Fewer',
-    description: "<p><strong>Hey, I'm Matt Fewer.</strong> I’m a software engineer and musician currently based in Berlin.</p><p>I'm currently a <strong>Senior Software Developer</strong> at Just Eat Takeaway.com. Before that, I was a programming teacher at SPICED Academy.</p><p>As a musician, performer, and music producer. I’ve worked with many original and cover acts throughout the New York, where I grew up.</p><p>When I’m not working, I enjoy snowboarding, reading, exercising, and lazing around with my wife and 2 cats.</p>",
-    tagline: 'Create. Learn. Share.',
-    image: '/static/images/matt-fewer-lilienstein.jpg',
-    gradientColor: 'pink-purple',
-    selectionColor: 'pink'
-  }
+    title: "About | Matt Fewer",
+    description:
+      "<p><strong>Hey, I'm Matt Fewer.</strong> I’m a software engineer and musician currently based in Berlin.</p><p>I'm currently a <strong>Senior Software Developer</strong> at Just Eat Takeaway.com. Before that, I was a programming teacher at SPICED Academy.</p><p>As a musician, performer, and music producer. I’ve worked with many original and cover acts throughout the New York, where I grew up.</p><p>When I’m not working, I enjoy snowboarding, reading, exercising, and lazing around with my wife and 2 cats.</p>",
+    tagline: "Create. Learn. Share.",
+    image: "/static/images/matt-fewer-lilienstein.jpg",
+    gradientColor: "pink-purple",
+    selectionColor: "pink"
+  };
 
-  return { props: meta }
+  return { props: meta };
 }
 
 function About(props) {
-  const { title, description, image } = props
+  const { title, description, image } = props;
 
   const renderIntro = () => {
-    return <div className="about">
-      <div className="about-section">
-        <Image
-          alt="Matt"
-          src="/static/images/matt-fewer-suit.JPG"
-          width="336"
-          height="336"
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAP0lEQVQImQE0AMv/AFBQUJKSkqmpqaOjowCurq7v7+/Jycm5ubkA////jIyMn5+fg4ODADAwMD09PWlpaQAAAApRGnEHblMWAAAAAElFTkSuQmCC"
-          priority
+    return (
+      <div className="about">
+        <div className="about-section">
+          <Image
+            alt="Matt"
+            src="/static/images/matt-fewer.jpg"
+            width="336"
+            height="336"
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAP0lEQVQImQE0AMv/AFBQUJKSkqmpqaOjowCurq7v7+/Jycm5ubkA////jIyMn5+fg4ODADAwMD09PWlpaQAAAApRGnEHblMWAAAAAElFTkSuQmCC"
+            priority
+          />
+        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: description }}
+          className="about-section"
         />
       </div>
-      <div
-        dangerouslySetInnerHTML={{ __html: description }}
-        className="about-section"
-      />
-    </div>
-  }
+    );
+  };
 
   const renderFeatured = () => {
     const featured = [
       "Designing APIs for 150 Million Orders",
-      "From Musician to Software Developer",
+      "From Musician to Software Developer"
     ];
 
     return videos
@@ -64,42 +67,49 @@ function About(props) {
 
   const renderAll = () => {
     return career.map((item, index) => {
-      return <div style={{ marginBottom: 40 }} key={index}>
-        <h3>{item.jobTitle}</h3>
-        <p style={{ margin: 0 }}>
-          <a href={item.companyUrl} target="_blank">{item.company}</a>
-          <span> • {item.location}</span>
-        </p>
-        <p style={{ margin: 0 }}>
-          <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
-          <span> – </span>
-          <span>{item.endDate ? format(parseISO(item.endDate), 'LLL yyyy') : 'Present'}</span>
-          <span> • </span>
-          <span>{getDuration(item.startDate, item.endDate)}</span>
-        </p>
-      </div>
-    })
-  }
+      return (
+        <div style={{ marginBottom: 40 }} key={index}>
+          <h3>{item.jobTitle}</h3>
+          <p style={{ margin: 0 }}>
+            <a href={item.companyUrl} target="_blank">
+              {item.company}
+            </a>
+            <span> • {item.location}</span>
+          </p>
+          <p style={{ margin: 0 }}>
+            <span>{format(parseISO(item.startDate), "LLL yyyy")}</span>
+            <span> – </span>
+            <span>
+              {item.endDate
+                ? format(parseISO(item.endDate), "LLL yyyy")
+                : "Present"}
+            </span>
+            <span> • </span>
+            <span>{getDuration(item.startDate, item.endDate)}</span>
+          </p>
+        </div>
+      );
+    });
+  };
 
   const getDuration = (startDate, endDate) => {
     const durationObj = intervalToDuration({
       start: parseISO(startDate),
       end: endDate ? parseISO(endDate) : new Date()
-    })
+    });
 
-    let durationStr = ''
+    let durationStr = "";
 
     if (durationObj.years > 1) {
-      durationStr = `${durationObj.years} yrs `
-    }
-    else if (durationObj.years === 1) {
-      durationStr = `${durationObj.years} yr `
+      durationStr = `${durationObj.years} yrs `;
+    } else if (durationObj.years === 1) {
+      durationStr = `${durationObj.years} yr `;
     }
 
-    durationStr += `${durationObj.months} mos`
+    durationStr += `${durationObj.months} mos`;
 
-    return durationStr
-  }
+    return durationStr;
+  };
 
   return (
     <div className="single">
@@ -114,6 +124,12 @@ function About(props) {
 
       {renderIntro()}
 
+      <h2 style={{ marginBottom: 40 }}>Want to get in touch?</h2>
+      <div>
+        Send me an email at{" "}
+        <a href="mailto:mattfewer@hotmail.com">mattfewer@hotmail.com</a>.
+      </div>
+
       <h2>Featured Videos</h2>
       {/* ToDo: change things like the classNames */}
       <div className="featured-talks">{renderFeatured()}</div>
@@ -121,9 +137,9 @@ function About(props) {
       <h2>Career</h2>
       {renderAll()}
     </div>
-  )
+  );
 }
 
-About.Layout = Main
+About.Layout = Main;
 
-export default About
+export default About;
